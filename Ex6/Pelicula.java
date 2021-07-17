aede4c8bff3f0e899b8564349762b0fd336b80b8
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-07-17 15:48:58 +0200

    Create Pelicula.java

---
+++ Ex6/Pelicula.java
@@ -0,0 +1,19 @@
+
+public class Pelicula {
+
+	protected String titulo;
+	protected int duracion;
+	protected int edad_minima;
+	protected String director;
+	
+	public Pelicula (String titulo, int duracion, int edad_minima, String director) {
+		this.titulo = titulo;
+		this.duracion = duracion;
+		this.edad_minima = edad_minima;
+		this.director=director;
+	}
+	
+	
+	
+	
+}
